f84860423f3f71f5b8d79847752a61e485d72c0f
evgen2892 <v9650639434@yandex.ru>
2026-02-28 20:32:21 +0300

    LR3

---
+++ ЛР3/task1.py
@@ -0,0 +1,28 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self.name = name
+        self.author = author
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
+
+class PaperBook(Book):
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
+
+
+class AudioBook(Book):
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    def __str__(self):
+        return f"Книга {self.name}. Автор {self.author}"
